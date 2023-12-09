35e0771540352f712ec4982dee3fd3c95d6c4389
Cordawg <elcorogrande@gmail.com>
2023-12-09 08:02:56 -0600

    DAY 4

---
+++ day4/myCodeDay4.py
@@ -0,0 +1,66 @@
+#!/usr/local/bin/python3
+import sys
+import os
+
+total = 0
+INPUT_FILE = "input_large.txt"
+
+# ---------------------------------------------------
+# GetWinners
+# ---------------------------------------------------
+def GetWinners(inString):
+   theWinners = []
+   #print (f"Anybody got these?? {inString}")
+   theWinners = inString.split(' ')
+
+   return theWinners
+#end GetWinners
+
+# ---------------------------------------------------
+# CalcWinningEntries
+# ---------------------------------------------------
+def CalcWinningEntries(winners, allEntries):
+   winnerCount = 0
+
+   theEntries = allEntries.split(' ')
+
+   for entry in theEntries:
+      #print (f"Look for {entry}")
+      filteredEntry = entry.strip()
+      if (len(filteredEntry) > 0):
+         if entry in winners: 
+            #
+            winnerCount = winnerCount + 1
+            if winnerCount > 2:
+               winnerCount = (winnerCount - 1) * 2
+            print(f"  winner <{entry}> - points: {winnerCount}")
+
+   return winnerCount
+
+#end CalcWinningEntries
+
+# ---------------------------------------------------
+# Main
+# ---------------------------------------------------
+script_path = os.path.realpath(__file__)
+script_dir = os.path.dirname(script_path)
+inputFilePath = os.path.join(script_dir, INPUT_FILE)
+
+winnerCollection = []
+distanceCollection = []
+
+index = 0
+with open(inputFilePath, 'r') as file:
+   for line in file:
+      index = index + 1
+      print(f"Parsing line {index}")
+      theLine = line.strip()
+      if theLine.startswith("Card"):
+         afterColon = theLine.split(":")[1].strip()
+
+         pipeSplits = afterColon.split("|")
+
+         winnerCollection = GetWinners(pipeSplits[0].strip())
+         total = total + CalcWinningEntries(winnerCollection, pipeSplits[1])
+
+print (f"TOTAL: {total}")
